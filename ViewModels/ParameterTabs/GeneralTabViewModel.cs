9ebccc232e7e5882ecad43c5136ecd1f86816609
Simon Coenen <sim2.coenen@gmail.com>
2017-01-01 11:20:37 +0100

    UX Extras

---
+++ ViewModels/ParameterTabs/GeneralTabViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using ParticleEditor.Model.Data;
+
+namespace ParticleEditor.ViewModels.ParameterTabs
+{
+    class GeneralTabViewModel : ViewModelBase
+    {
+        public bool EmitFromShellEnabled
+        {
+            get
+            {
+                return (MainViewModel.MainParticleSystem.Shape.ShapeType == ParticleSystem.ShapeType.CIRCLE ||
+                        MainViewModel.MainParticleSystem.Shape.ShapeType == ParticleSystem.ShapeType.CONE ||
+                        MainViewModel.MainParticleSystem.Shape.ShapeType == ParticleSystem.ShapeType.SPHERE);
+            }
+        }
+
+        public bool EmitFromVolumeEnabled
+        {
+            get
+            {
+                return (MainViewModel.MainParticleSystem.Shape.ShapeType == ParticleSystem.ShapeType.CONE ||
+                        MainViewModel.MainParticleSystem.Shape.ShapeType == ParticleSystem.ShapeType.SPHERE);
+            }
+        }
+
+        public bool AngleEnabled
+        {
+            get { return MainViewModel.MainParticleSystem.Shape.ShapeType == ParticleSystem.ShapeType.CONE; }
+        }
+
+        public RelayCommand OnShapeChangedCommand => new RelayCommand(() =>
+        {
+            RaisePropertyChanged("EmitFromShellEnabled");
+            RaisePropertyChanged("EmitFromVolumeEnabled");
+            RaisePropertyChanged("AngleEnabled");
+        });
+    }
+}
